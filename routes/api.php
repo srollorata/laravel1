b939515a16e2cce32217cf616ffa3c301717f99c
wyn327 <selwynrollorata14@gmail.com>
2023-10-24 14:43:35 +0800

    Added back the normal way of routings in api. And commented the resource routing

--- routes/api.php
+++ routes/api.php
@@ -19,12 +19,24 @@ use App\Http\Controllers\Api\CarouselItemsController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
 // For carousel
-Route::resource('carousel', CarouselItemsController::class);
+// Route::resource('carousel', CarouselItemsController::class);
+
+// For user
+// Route::resource('carousel', CarouselItemsController::class);
+
+Route::get('/carousel', [CarouselItemsController::class, 'index']);
+Route::get('/carousel/{id}', [CarouselItemsController::class, 'show']);
+Route::delete('/carousel/{id}', [CarouselItemsController::class, 'destroy']);
+Route::post('/carousel', [CarouselItemsController::class, 'store']);
+Route::put('/carousel/{id}', [CarouselItemsController::class, 'update']);
 
 Route::get('/user', [UserController::class, 'index']);
+Route::get('/user/{id}', [UserController::class, 'show']);
 Route::delete('/user/{id}', [UserController::class, 'destroy']);
 Route::post('/user', [UserController::class, 'store']);
+Route::put('/user/{id}', [UserController::class, 'update']);
 
 
 Route::get('/greeting', function () {
